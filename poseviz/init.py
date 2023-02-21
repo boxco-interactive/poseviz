85e5447b04ff647dd4e9ffd10c9b590335f244b8
Nicolas de Maubeuge <nicolas.de.maubeuge@joinboxco.com>
2023-02-21 18:36:47 +0100

    Change window name to BoxCo

--- poseviz/init.py
+++ poseviz/init.py
@@ -3,7 +3,7 @@ from mayavi import mlab
 from tvtk.api import tvtk
 
 def initialize(size=(1280, 720)):
-    fig = mlab.figure('PoseViz', bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=size)
+    fig = mlab.figure('BoxCo Prototype: Pose Estimation', bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=size)
     fig.scene.renderer.render_window.set(alpha_bit_planes=1, multi_samples=0, full_screen=False)
     fig.scene.renderer.set(
         use_depth_peeling=True, maximum_number_of_peels=5, occlusion_ratio=0.01)

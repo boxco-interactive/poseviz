1b6e8542e072bfc0e43d1e73e48f046da32c2395
Nicolas de Maubeuge <nicolas.de.maubeuge@joinboxco.com>
2023-02-23 12:04:10 +0100

    Modify squeletons display with colors and different sizes

--- poseviz/components/skeletons_viz.py
+++ poseviz/components/skeletons_viz.py
@@ -1,5 +1,6 @@
 import poseviz.components.connected_points_viz
 import poseviz.mayavi_util
+import poseviz.colors as colors
 
 
 class SkeletonsViz:
@@ -7,13 +8,9 @@ class SkeletonsViz:
                  high_quality=False):
         opacity = 0.7
         joint_names, joint_edges = joint_info
-        edge_colors = dict(left=left_color, mid=mid_color, right=right_color)
-        sides = ('left', 'mid', 'right')
-        self.pointsets = {
-            side: poseviz.components.connected_points_viz.ConnectedPoints(
-                point_color, edge_colors[side], 'sphere', scale_factor, opacity, opacity,
-                joint_names, high_quality)
-            for side in sides}
+        edge_colors = dict(left=left_color, mid=mid_color, right=right_color, fist=(1.0,0.0,0.0), head=(0.940, 0.0658, 0.765))
+        point_colors = dict(left=point_color, mid=point_color, right=point_color, fist=(1.0,0.0,0.0), head=(0.940, 0.0658, 0.765))
+        sides = ('left', 'mid', 'right', 'fist', 'head')
 
         def edge_side(edge):
             s1 = joint_side(edge[0])
@@ -27,12 +24,30 @@ class SkeletonsViz:
 
         def joint_side(joint):
             name = joint_names[joint].lower()
-            if name.startswith('l'):
+            if name.endswith('head'):
+                return 'head'
+            if name.endswith('wrist'):
+                return 'fist'
+            elif name.startswith('l'):
                 return 'left'
             elif name.startswith('r'):
                 return 'right'
             else:
                 return 'mid'
+            
+        # compute join sides
+        joints_of_joint_side = {}
+        for side in sides:
+            joints_of_joint_side[side] = [joint_name for joint_name in joint_names if joint_side(joint_names.index(joint_name)) == side]
+
+        scale_factor_of_joint_side = {'left': scale_factor, 'mid': scale_factor, 'right': scale_factor, 'fist': 2*scale_factor, 'head': 2.5*scale_factor}
+        
+        # this is where the color stuff happens
+        self.pointsets = {
+            side: poseviz.components.connected_points_viz.ConnectedPoints(
+                point_colors[side], edge_colors[side], 'sphere', scale_factor_of_joint_side[side], opacity, opacity,
+                joints_of_joint_side[side], high_quality)
+            for side in sides}
 
         self.edges_per_side = {
             side: [e for e in joint_edges if edge_side(e) == side]
